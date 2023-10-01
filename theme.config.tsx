ce856ce31931e2e977db64aae52e7d2141adb597
alexturcoo <alexanderturco1@gmail.com>
2023-10-01 16:04:37 -0400

    Updates

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,14 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Cells at War</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/alexturcoo/cellsatwar-documentation',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  
+  docsRepositoryBase: 'https://github.com/alexturcoo/cellsatwar-documentation',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Made by the Cells at War Team',
   },
 }
 
